135517480737536f3e29c1db8f4763b342a1f4a8
yowfeng.nyon <yowfeng.nyon@gmail.com>
2021-02-10 10:10:20 +0800

    tried boyer moore algo for string search

--- 01-Easy/implement_strstr/implement_strstr.py
+++ 01-Easy/implement_strstr/implement_strstr.py
@@ -28,26 +28,77 @@
 
 
 class Solution:
+    # https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_string-search_algorithm
+    # Boyer Moore Search string method
     def strStr(self, haystack: str, needle: str) -> int:
         len_needle = len(needle)
         
         if len_needle == 0:
             return 0
-        
+
         len_hay = len(haystack)
-        
-        
-        for i in range(len_hay - len_needle + 1):
-            # we have a chance, try to find the rest of the string
-            if haystack[i] == needle[0]:
-                found = True
-                for j in range(1, len_needle):
-                    if haystack[i+j] != needle[j]:
-                        found = False
-                        break
-                
-                if found:
-                    return i
-                
-        
+        i = 0
+        while i <= (len_hay - len_needle) :
+            j = len_needle - 1
+            while j >= 0:
+                if haystack[i + j] != needle[j]:
+                    #search haystack[i+j] if it exists in needle anywhere
+                    k = j - 1
+                    while k >= 0:
+                        if haystack[i + j] == needle[k]:
+                            i = i + j - k - 1
+                            break
+                        k -= 1
+                    if k < 0:
+                        i = i + j
+                    break
+                j -= 1
+
+            if j < 0:
+                return i
+            i += 1
+
         return -1
+
+    # brute force method
+    # def strStr(self, haystack: str, needle: str) -> int:
+    #     len_needle = len(needle)
+        
+    #     if len_needle == 0:
+    #         return 0
+        
+    #     len_hay = len(haystack)
+        
+        
+    #     for i in range(len_hay - len_needle + 1):
+    #         # we have a chance, try to find the rest of the string
+    #         if haystack[i] == needle[0]:
+    #             found = True
+    #             for j in range(1, len_needle):
+    #                 if haystack[i+j] != needle[j]:
+    #                     found = False
+    #                     break
+                
+    #             if found:
+    #                 return i
+                
+        
+    #     return -1
+
+def checkAnswer(s:Solution, haystack: str, needle: str, ans):
+    output = s.strStr(haystack, needle)
+    if ans != output: 
+        print("Wrong answer")
+        print("output:", output)
+        print("answer:", ans)
+        assert(False)
+
+if __name__ == "__main__":
+    s = Solution()
+    checkAnswer(s, "ababbbbaaabbbaaa", "bbbb", 3)
+
+    checkAnswer(s, "mississippi", "issip", 4)
+    checkAnswer(s, "hello", "ll", 2)
+    checkAnswer(s, "aaaaa", "bba", -1)
+    checkAnswer(s, "", "", 0)
+    
